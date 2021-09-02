abf4abb2cd3858b5a883838b783905c2717b993b
MeghaWhat <meghamehreenrahman@gmail.com>
2021-09-02 21:40:04 +0800

    initial review set-up done

---
+++ public/js/review.js
@@ -0,0 +1,48 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const content = document.querySelector('#content').value.trim();
+//    const user_id = ;
+//    const book_id = ;
+//    const book_title = ;
+
+    if ( content ) {
+        const response = await fetch(`/api/book`, {
+            method: 'POST',
+            body: JSON.stringify({user_id, book_id, content }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/book/:id')
+        } else {
+            alert('Failed to post review');
+        }
+    }
+};
+
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch(`/api/book/review/${id}`, {
+            method: 'DELETE',
+          });
+      
+          if (response.ok) {
+            document.location.replace('/book/:id');
+          } else {
+            alert('Failed to delete review');
+        }
+    }
+};
+
+document
+  .querySelector('.review-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.review-list')
+  .addEventListener('click', delButtonHandler);
