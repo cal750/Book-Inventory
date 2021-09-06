9158d7c5ef442f8a3598161b6757534fec6c1f0a
SV <scottyv6@gmail.com>
2021-09-06 19:30:58 +0800

    split singup and login to two pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -77,11 +77,17 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/');
     return;
   }
 
   res.render('login');
+});
+
+// register
+router.get('/register', function(req, res){
+
+  res.render('register');
 });
 
 module.exports = router;
